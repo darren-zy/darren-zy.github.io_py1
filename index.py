c7a2c9859f65d409565843c5af436e91368fc73a
darren_zy <799516439@qq.com>
2022-08-03 11:43:25 +0800

    first commit

---
+++ index.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Author : darren
+
+"""
+source env/bin/activate
+
+"""
+
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write("DearXuan's API by python!".encode())
+        return
+
+
+print(handler)
