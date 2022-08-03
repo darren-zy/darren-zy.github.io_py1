e600d151364cc3109b5457885f624f1ce45430d0
darren_zy <799516439@qq.com>
2022-08-03 11:47:03 +0800

    first commit

--- index.py
+++ index.py
@@ -19,3 +19,10 @@ class handler(BaseHTTPRequestHandler):
 
 
 print(handler)
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(self.headers.get('x-forwarded-for').encode())
+        return
